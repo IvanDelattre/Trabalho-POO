d20ee27d4fa01764e2c971614cf8d60b6974869f
csbassin <cauabassin@id.uff.br>
2024-12-15 16:31:12 -0300

    segunda reunião

---
+++ src/main/java/com/carlosribeiro/dao/PedidoDAO.java
@@ -0,0 +1,7 @@
+package com.carlosribeiro.dao;
+
+import com.carlosribeiro.model.Pedido;
+
+public interface PedidoDAO extends DAOGenerico<Pedido>  {
+
+}
---
+++ src/main/java/com/carlosribeiro/dao/impl/DAOPedidoImpl.java
@@ -0,0 +1,8 @@
+package com.carlosribeiro.dao.impl;
+
+import com.carlosribeiro.dao.PedidoDAO;
+import com.carlosribeiro.model.Pedido;
+
+public class DAOPedidoImpl extends DAOGenericoImpl<Pedido> implements PedidoDAO {
+
+}
